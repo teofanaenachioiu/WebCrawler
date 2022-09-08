7012beb84702952f1f97a7715e8c5e57a4f6b7a3
Teofana Enachioiu <teofanaenachioiu@gmail.com>
2022-09-08 20:03:25 +0300

    first stable version

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,33 +7,38 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Queue;
 import java.util.Set;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 //https://www.linkedin.com/pulse/selenium-parallel-testing-using-java-threadlocal-testng-shargo/
 //https://github.com/bonigarcia/webdrivermanager
 
+
 public class Main {
     private static final String HTTPS = "https://";
     private static final String HTTP = "http://";
-    private static final String BASE_URL = "tomblomfield.com";
+    private static final String BASE_URL = "tomblomfield.com/";
     private static final String BASE_URL_HTTPS = HTTPS + BASE_URL;
     private static final String BASE_URL_HTTP = HTTP + BASE_URL;
     private static final Set<String> foundLinks = new HashSet<>();
     private static final Queue<LinkNode> processingLinks = new ConcurrentLinkedQueue<>();
+    private static final ExecutorService executorService = Executors.newFixedThreadPool(5);
     private static WebDriver driver;
     private static ThreadLocal<WebDriver> threadLocal = new ThreadLocal<>();
 
-    private static Set<String> getRelativeLinks() {
-        return driver.findElements(By.tagName("a")).stream()
+    private static boolean isLinkAccessibleFromBaseUrl(String link) {
+        return link.startsWith(BASE_URL_HTTPS) || link.startsWith(BASE_URL_HTTP);
+    }
+
+    private static Set<String> getAllLinks(WebDriver webDriver) {
+        return webDriver.findElements(By.tagName("a")).stream()
                 .map(e -> e.getAttribute("href"))
-                .filter(elem -> {
-                    if (elem != null) {
-                        return elem.startsWith(BASE_URL_HTTPS) || elem.startsWith(BASE_URL_HTTP);
-                    }
-                    return false;
-                })
+                .filter(Objects::nonNull)
                 .map(e -> {
                     int hashPosition = e.lastIndexOf("#");
                     if (hashPosition > -1) {
@@ -44,91 +49,66 @@ public class Main {
                 .collect(Collectors.toSet());
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
 
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("start-maximized"); // open Browser in maximized mode
+//        options.addArguments("start-maximized"); // open Browser in maximized mode
         options.addArguments("--headless"); // without this it won't start on servers
 
         driver = new ChromeDriver(options);
 
-//        foundLinks.add(BASE_URL_HTTPS);
-//        foundLinks.add(BASE_URL_HTTP);
+        foundLinks.add(BASE_URL_HTTPS);
+        foundLinks.add(BASE_URL_HTTP);
 
         LinkNode root = new LinkNode(BASE_URL_HTTPS);
         processingLinks.add(root);
 
+//        && Thread.getAllStackTraces().keySet().stream().filter(t -> t.equals(Thread.currentThread())).anyMatch(Thread::isAlive)
         while (!processingLinks.isEmpty()) {
             LinkNode currentLinkNode = processingLinks.poll();
-
             driver.get(currentLinkNode.getLink());
-            currentLinkNode.setLink(driver.getCurrentUrl());
 
-            System.out.println("currentLinkHierarchy : " + currentLinkNode.getHierarchy());
+            System.out.println("processing " + currentLinkNode.getLink());
 
-            for (String link : getRelativeLinks()) {
-                if (!currentLinkNode.existsLinkInHierarchy(link)) {
-//                    foundLinks.add(link);
-                    LinkNode child = new LinkNode(link, currentLinkNode.getHierarchy());
-//                    currentLinkNode.getChildren().add(child);
-//                    currentLinkNode.getJson().add(link, child.getJson());
+            for (String link : getAllLinks(driver)) {
+                if (!isLinkAccessibleFromBaseUrl(link)) {
+                    currentLinkNode.addJsonProperty(link, new JsonObject());
+                } else if (!foundLinks.contains(link)) {
+                    LinkNode child = new LinkNode(link);
+                    currentLinkNode.addJsonProperty(link, child.getJson());
+
+                    foundLinks.add(link);
                     processingLinks.add(child);
                 }
             }
         }
 
-//        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-//        System.out.println(gson.toJson(root.getJson()));
-
         driver.quit();
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        System.out.println(gson.toJson(root.getJson()));
 
     }
 
     private static class LinkNode {
         private String link;
-        //        private List<LinkNode> children = new ArrayList<>();
-//        private JsonObject json = new JsonObject();
-        private Set<String> hierarchy;
+        private JsonObject json = new JsonObject();
 
         public LinkNode(String link) {
             this.link = link;
-            this.hierarchy = new HashSet<>();
-            this.hierarchy.add(link);
         }
-
-        public LinkNode(String link, Set<String> hierarchy) {
-            this.link = link;
-            this.hierarchy = new HashSet<>(hierarchy);
-            this.hierarchy.add(link);
-        }
-
-//        public List<LinkNode> getChildren() {
-//            return children;
-//        }
 
         public String getLink() {
             return link;
         }
 
-//        public JsonObject getJson() {
-//            return json;
-//        }
-
-        public Set<String> getHierarchy() {
-            return hierarchy;
+        public JsonObject getJson() {
+            return json;
         }
 
-        public void setLink(String link) {
-            if (!this.link.equals(link)) {
-                this.hierarchy.remove(this.link);
-                this.link = link;
-                this.hierarchy.add(link);
-            }
-        }
-
-        public boolean existsLinkInHierarchy(String linkToBeChecked) {
-            return hierarchy.contains(linkToBeChecked);
+        public void addJsonProperty(String key, JsonObject json) {
+            this.json.add(key, json);
         }
     }
 }
