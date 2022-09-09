ea6a47fbb4abb70e838ae79e39422694f6f64d2b
Teofana Enachioiu <teofanaenachioiu@gmail.com>
2022-09-09 15:50:36 +0300

    parallel version

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,30 +7,27 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.util.HashSet;
 import java.util.Objects;
 import java.util.Queue;
 import java.util.Set;
-import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 import java.util.stream.Collectors;
-//https://www.linkedin.com/pulse/selenium-parallel-testing-using-java-threadlocal-testng-shargo/
-//https://github.com/bonigarcia/webdrivermanager
-
 
 public class Main {
+    private static final int NUM_THREADS = (int) Math.floor(0.8 * Runtime.getRuntime().availableProcessors());
     private static final String HTTPS = "https://";
     private static final String HTTP = "http://";
-    private static final String BASE_URL = "tomblomfield.com/";
+    private static final String BASE_URL = "tomblomfield.com";
     private static final String BASE_URL_HTTPS = HTTPS + BASE_URL;
     private static final String BASE_URL_HTTP = HTTP + BASE_URL;
-    private static final Set<String> foundLinks = new HashSet<>();
-    private static final Queue<LinkNode> processingLinks = new ConcurrentLinkedQueue<>();
-    private static final ExecutorService executorService = Executors.newFixedThreadPool(5);
-    private static WebDriver driver;
-    private static ThreadLocal<WebDriver> threadLocal = new ThreadLocal<>();
+
+    private static final ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool(NUM_THREADS);
+    private static final ThreadLocal<WebDriver> threadLocal = new ThreadLocal<>();
+    private static final Semaphore semaphore = new Semaphore(1);
+
+    private static volatile Queue<WebDriver> threadDrivers = new ConcurrentLinkedQueue<>();
+    private static volatile Set<String> foundLinks = new ConcurrentSkipListSet<>();
+    private static volatile Queue<LinkNode> processingLinks = new ConcurrentLinkedQueue<>();
 
     private static boolean isLinkAccessibleFromBaseUrl(String link) {
         return link.startsWith(BASE_URL_HTTPS) || link.startsWith(BASE_URL_HTTP);
@@ -50,11 +47,14 @@ public class Main {
                 .collect(Collectors.toSet());
     }
 
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println(NUM_THREADS + " threads");
+
+        long startTime = System.nanoTime();
+
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
-        driver = new ChromeDriver(options);
 
         foundLinks.add(BASE_URL_HTTPS);
         foundLinks.add(BASE_URL_HTTP);
@@ -62,36 +62,54 @@ public class Main {
         LinkNode root = new LinkNode(BASE_URL_HTTPS);
         processingLinks.add(root);
 
-//        && Thread.getAllStackTraces().keySet().stream().filter(t -> t.equals(Thread.currentThread())).anyMatch(Thread::isAlive)
-        while (!processingLinks.isEmpty()) {
-            LinkNode currentLinkNode = processingLinks.poll();
-            driver.get(currentLinkNode.getLink());
-
-            System.out.println("processing " + currentLinkNode.getLink());
-
-            for (String link : getAllLinks(driver)) {
-                if (!isLinkAccessibleFromBaseUrl(link)) {
-//                    currentLinkNode.addJsonProperty(link, new JsonObject());
-                } else if (!foundLinks.contains(link)) {
-                    LinkNode child = new LinkNode(link);
-                    currentLinkNode.addJsonProperty(link, child.getJson());
-
-                    foundLinks.add(link);
-                    processingLinks.add(child);
-                }
+        while (!processingLinks.isEmpty() || threadPoolExecutor.getActiveCount() != 0) {
+            if (threadPoolExecutor.getActiveCount() == NUM_THREADS) {
+                semaphore.acquire();
             }
+
+            threadPoolExecutor.submit(() -> {
+                LinkNode currentLinkNode = processingLinks.poll();
+
+                if (currentLinkNode == null) return;
+
+                if (threadLocal.get() == null) {
+                    WebDriver threadWebDriver = new ChromeDriver(options);
+                    threadDrivers.add(threadWebDriver);
+                    threadLocal.set(threadWebDriver);
+                }
+                WebDriver driver = threadLocal.get();
+                driver.get(currentLinkNode.getLink());
+
+                for (String link : getAllLinks(driver)) {
+                    if (!isLinkAccessibleFromBaseUrl(link)) {
+                        currentLinkNode.addJsonProperty(link, new JsonObject());
+                    } else if (!foundLinks.contains(link)) {
+                        LinkNode child = new LinkNode(link);
+                        currentLinkNode.addJsonProperty(link, child.getJson());
+
+                        foundLinks.add(link);
+                        processingLinks.add(child);
+                    }
+                }
+
+                semaphore.release();
+            });
+
         }
 
-        driver.quit();
+        threadDrivers.forEach(WebDriver::quit);
+        threadPoolExecutor.shutdown();
 
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         System.out.println(gson.toJson(root.getJson()));
 
+        long stopTime = System.nanoTime();
+        System.out.println(((double) (stopTime - startTime) / 1_000_000_000L) + " seconds");
     }
 
     private static class LinkNode {
-        private String link;
-        private JsonObject json = new JsonObject();
+        private final String link;
+        private final JsonObject json = new JsonObject();
 
         public LinkNode(String link) {
             this.link = link;
@@ -110,71 +128,3 @@ public class Main {
         }
     }
 }
-
-
-//    public static WebDriver doBrowserSetup(String browserName){
-//
-//        WebDriver driver = null;
-//        if (browserName.equalsIgnoreCase("chrome")){
-//            //steup chrome browser
-//            WebDriverManager.chromedriver().setup();
-//
-//
-//            //Add options for --headed or --headless browser launch
-//            ChromeOptions chromeOptions = new ChromeOptions();
-//            chromeOptions.addArguments("-headed");
-//
-//            //initialize driver for chrome
-//            driver = new ChromeDriver(chromeOptions);
-//
-//            //maximize window
-//            driver.manage().window().maximize();
-//
-//            //add implicit timeout
-//            driver.manage()
-//                    .timeouts()
-//                    .implicitlyWait(Duration.ofSeconds(30));
-//
-//        }
-//        return driver;
-//    }
-
-
-//    ExecutorService executorService = Executors.newFixedThreadPool(5);
-
-
-// for(String link : links) {
-//         executorService.submit(() -> {
-//         if (threadLocal.get() == null)
-//         threadLocal.set(new ChromeDriver(options));
-//         WebDriver driver2 = threadLocal.get();
-//
-//         driver2.get(link);
-//         System.out.println("BBB:" + driver2.getCurrentUrl());
-//
-//         Set<String> link2 = driver2.findElements(By.tagName("a")).stream()
-//        .map(e -> e.getAttribute("href"))
-//        .filter(elem -> {
-//        if (elem != null) {
-//        return elem.startsWith(BASE_URL_HTTPS) || elem.startsWith(BASE_URL_HTTP);
-//        }
-//        return false;
-//        })
-//        .map(e -> {
-//        int hashPosition = e.lastIndexOf("#");
-//        if (hashPosition > -1) {
-//        return e.substring(0, hashPosition);
-//        }
-//        return e;
-//        })
-//        .collect(Collectors.toSet());
-//
-//        link2.forEach(System.out::println);
-//        System.out.println("---");
-//
-//        System.out.println();
-//
-//        driver.quit();
-//        });
-//        }
-//        executorService.shutdown();
